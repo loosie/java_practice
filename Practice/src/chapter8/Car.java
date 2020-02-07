61e55e4c6ca79615a3d062269ae36aa458974796
loosie <jong9712@naver.com>
2020-02-07 19:02:39 +0900

    ch8 end

---
+++ Practice/src/chapter8/Car.java
@@ -0,0 +1,18 @@
+package chapter8;
+
+public abstract class Car {
+
+	public abstract void startCar();
+	public abstract void drive(); 
+	public abstract void stop();
+	public abstract void turnOff();
+	
+	
+	
+	final public void run() {
+		startCar();
+		drive();
+		stop();
+		turnOff();
+	}
+}
