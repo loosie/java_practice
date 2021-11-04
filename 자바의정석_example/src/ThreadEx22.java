ac064cbb09f7fe391ec7830955e6d4b12bfc68a0
jongwon <54282927+loosie@users.noreply.github.com>
2021-11-05 01:43:20 +0900

    #놓치기 쉬운 자바 실전 - 티쓰레드의 동기화(synchronized, wait, notify)

--- 자바의정석_example/src/ThreadEx22.java
+++ 자바의정석_example/src/ThreadEx22.java
@@ -1,20 +1,20 @@
-// synchronized x
-public class ThreadEx21 {
+// synchronized o
+public class ThreadEx22 {
     public static void main(String[] args){
-        Runnable r = new RunnableEx21();
+        Runnable r = new RunnableEx22();
         new Thread(r).start(); // ThreadGroup()에 의해 참조되므로 gc대상이 아니다.
         new Thread(r).start(); // ThreadGroup()에 의해 참조되므로 gc대상이 아니다.
     }
 }
 
-class Account {
-    private int balance = 1000;
+class Account2 {
+    private int balance = 1000; // private으로 해야 동기화가 의미가 있다.
 
     public int getBalance() {
         return balance;
     }
 
-    public void withdraw(int money){
+    public synchronized void withdraw(int money){ // synchronized 메서드로 동기화
         if(balance >= money){
             try{
                 Thread.sleep(1000);
@@ -25,8 +25,8 @@ class Account {
 }
 
 
-class RunnableEx21 implements Runnable {
-    Account acc = new Account();
+class RunnableEx22 implements Runnable {
+    Account2 acc = new Account2();
     public void run() {
         while(acc.getBalance()>0){
             // 100, 200, 300중의 한 값을 임의로 선택해서 출금(withdraw)
@@ -34,6 +34,5 @@ class RunnableEx21 implements Runnable {
             acc.withdraw(money);
             System.out.println("balance:"+acc.getBalance());
         }
-
     }
 }
