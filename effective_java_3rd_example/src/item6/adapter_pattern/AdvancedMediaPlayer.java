7f689601d21cc07061f19cb0847f7d36ae10cea9
jongwon <54282927+loosie@users.noreply.github.com>
2021-11-30 15:53:30 +0900

    effectiveJava #item6 사전 공부 - 어댑터(Adapter) 패턴 공부

---
+++ effective_java_3rd_example/src/item6/adapter_pattern/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package item6.adapter_pattern;
+
+public interface AdvancedMediaPlayer {
+	void playMP4(String filename);
+	void playMKV(String filename);
+}
---
+++ effective_java_3rd_example/src/item6/adapter_pattern/MediaPlayer.java
@@ -0,0 +1,5 @@
+package item6.adapter_pattern;
+
+public interface MediaPlayer {
+	void play(String type, String filename);
+}
