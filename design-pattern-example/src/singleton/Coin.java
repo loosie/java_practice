92269575261817ff3fb1dbc6c342983421249bfb
jongwon <54282927+loosie@users.noreply.github.com>
2022-06-15 01:21:50 +0900

    singleton: 정적 블럭 초기화 (인스턴스 낭비 방지)

--- design-pattern-example/src/singleton/Coin.java
+++ design-pattern-example/src/singleton/Coin.java
@@ -4,13 +4,17 @@ public class Coin {
 
 	private static final int ADD_MORE_COIN = 10;
 	private int coin;
-	private static Coin instance = new Coin();
+	private static Coin instance;
 
 	private Coin(){
 		// private 생성자로 선언하여 상속을 방지한다.
 	}
 
+	// 정적 블럭 초기화
 	public static Coin getInstance(){
+		if(instance == null){
+			instance = new Coin();
+		}
 		return instance;
 	}
 
@@ -22,8 +26,15 @@ public class Coin {
 		coin += ADD_MORE_COIN;
 	}
 
-	public void deductCoin(){
-		coin--;
+	// insertCoin이 현재 코인보다 적거나 같은 경우에만 동작한다.
+	public void deductCoin(int insertCoin){
+		if(coin >= insertCoin){
+			try{
+				Thread.sleep(1000);
+			}catch (InterruptedException ex){
+			}
+			coin -= insertCoin;
+		}
 	}
 }
 
